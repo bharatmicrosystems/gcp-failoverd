c8a9c6d6206e1adde6333a0c9483979a2e62e613
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-13 20:11:36 +0100

    Updated cloud NAT

--- scripts/gcp-failoverd.sh
+++ scripts/gcp-failoverd.sh
@@ -13,23 +13,23 @@ done
 mkdir -p /etc/gcp-failoverd
 #Check if the VIP is being used
 if $internal; then
-    INTERNAL_IP=`gcloud compute addresses list --filter=\"name=$internal_vip\"| grep $internal_vip | awk '{ print $2 }'`
+    INTERNAL_IP=`gcloud compute addresses list --filter="name=$internal_vip"| grep $internal_vip | awk '{ print $2 }'`
 fi
 if $external; then
-    EXTERNAL_IP=`gcloud compute addresses list --filter=\"name=$external_vip\"| grep $external_vip | awk '{ print $2 }'`
+    EXTERNAL_IP=`gcloud compute addresses list --filter="name=$external_vip"| grep $external_vip | awk '{ print $2 }'`
 fi
 while true; do
   if $internal; then
-      INTERNAL_IP_STATUS=`gcloud compute addresses list --filter=\"name=$internal_vip\"| grep $internal_vip | awk '{ print $NF }'`
+      INTERNAL_IP_STATUS=`gcloud compute addresses list --filter="name=$internal_vip"| grep $internal_vip | awk '{ print $NF }'`
   fi
   if $external; then
-      EXTERNAL_IP_STATUS=`gcloud compute addresses list --filter=\"name=$external_vip\"| grep $external_vip | awk '{ print $NF }'`
+      EXTERNAL_IP_STATUS=`gcloud compute addresses list --filter="name=$external_vip"| grep $external_vip | awk '{ print $NF }'`
   fi
   if [[ $INTERNAL_IP_STATUS == "IN_USE" ]];
   then
     echo "Internal IP address in use at $(date)" >> /etc/gcp-failoverd/poll.log
   else
-    ZONE=`gcloud compute instances list --filter=\"name=$(hostname)\"| grep $(hostname) | awk '{ print $2 }'`
+    ZONE=`gcloud compute instances list --filter="name=$(hostname)"| grep $(hostname) | awk '{ print $2 }'`
     # Assign IP aliases to me because now I am the MASTER!
     gcloud compute instances network-interfaces update $(hostname) \
       --zone $ZONE \
@@ -40,7 +40,7 @@ while true; do
   then
     echo "External IP address in use at $(date)" >> /etc/gcp-failoverd/poll.log
   else
-    ZONE=`gcloud compute instances list --filter=\"name=$(hostname)\"| grep $(hostname) | awk '{ print $2 }'`
+    ZONE=`gcloud compute instances list --filter="name=$(hostname)"| grep $(hostname) | awk '{ print $2 }'`
     # Assign IP aliases to me because now I am the MASTER!
     gcloud compute instances add-access-config $(hostname) \
      --access-config-name "$(hostname)-access-config" --address $EXTERNAL_IP_STATUS >> /etc/gcp-failoverd/takeover.log 2>&1
